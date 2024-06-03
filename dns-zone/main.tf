0d3ec8e29c76372f36885c759e684c88366a7f26
Javier Cabrera <javcabr@gmail.com>
2024-06-03 15:41:52 -0700

    formatting

--- dns-zone/main.tf
+++ dns-zone/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "main" {
-  name = var.domain
+  name          = var.domain
   force_destroy = true
 
   tags = {
--- dns-zone/outputs.tf
+++ dns-zone/outputs.tf
@@ -1,20 +1,20 @@
 locals {
-  full_list = concat(["Your list of name servers:"], aws_route53_zone.main.name_servers)
+  full_list   = concat(["Your list of name servers:"], aws_route53_zone.main.name_servers)
   full_string = join("\n", local.full_list)
 }
 
 output "name-servers-hr" {
   description = "Human readable names servers list"
-  value = local.full_string
+  value       = local.full_string
 }
 
 output "name-servers" {
   description = "name servers for domain name"
-  value = aws_route53_zone.main.name_servers
+  value       = aws_route53_zone.main.name_servers
 }
 
 output "zone-id" {
   description = "name servers for domain name"
-  value = aws_route53_zone.main.zone_id
+  value       = aws_route53_zone.main.zone_id
 }
 
