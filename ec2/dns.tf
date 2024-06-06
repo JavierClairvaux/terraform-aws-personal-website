724f0023d1966d652afa05a812c2919175d50cab
Javier Cabrera <javcabr@gmail.com>
2024-06-06 10:57:51 -0700

    initial dns setup

---
+++ ec2/dns.tf
@@ -0,0 +1,23 @@
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+  config = {
+    bucket = var.bucket
+    key    = "dns/dns-zone"
+    region = var.region
+  }
+}
+
+resource "aws_eip" "personal-eip" {}
+
+resource "aws_eip_association" "eip-assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.personal-eip.id
+}
+
+resource "aws_route53_record" "website-domain" {
+  zone_id = data.terraform_remote_state.dns.outputs.zone-id
+  name    = "test.${data.terraform_remote_state.dns.outputs.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.personal-eip.public_ip]
+}
