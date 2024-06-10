1757fe40042906ac17ba1289b3cfb32a841abba4
Javier Cabrera <javcabr@gmail.com>
2024-06-10 14:06:00 -0700

    user data file

---
+++ ec2/templates/user-data.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+while [ `curl http://checkip.amazonaws.com` != `dig +short test.cabreragon.xyz` ]
+do
+	echo "Not ready yet..."
+done
+
+echo """
+- name: 'Provision Image'
+  hosts: localhost
+  connection: local
+  become: true
+  roles:
+    - role: certbot
+      vars:
+       domain: "${domain}"
+       subdomain: "${subdomain}"
+       email: "${email}"
+
+""" > playbook.yml
+
+ansible-playbook playbook.yml
+
